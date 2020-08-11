15f63da6f54330b82fa1b6bb78e1c338734dcb3b
oussamabadr <oussamabadr@gmail.com>
2020-08-11 10:25:17 +0200

    + First commit

---
+++ src/main/java/org/badrfoot/gradle/releaseplugin/DemoReleasePluginApplication.java
@@ -0,0 +1,13 @@
+package org.badrfoot.gradle.releaseplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoReleasePluginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoReleasePluginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/badrfoot/gradle/releaseplugin/DemoReleasePluginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.badrfoot.gradle.releaseplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoReleasePluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
